113bd224b090c94637c81027e4c02688b9d89244
Bertil Braun <bertil.braun.private@gmail.com>
2024-08-26 21:49:47 +0200

    Update periodic_scraper.ps1

--- periodic_scraper.ps1
+++ periodic_scraper.ps1
@@ -26,28 +26,23 @@ function Main {
 
         Write-Output "Current Date: $currentDate"
         Write-Output "Yesterday's Date: $yesterdayDate"        
-	Write-Output "Current Hour: $currentHour"
+	    Write-Output "Current Hour: $currentHour"
 
         if (Test-Path $lastRunFile) {
             $lastRunDate = Get-Content $lastRunFile
             Write-Output "Last Run Date: $lastRunDate"
         } else {
-            Write-Output "No last run date file found, creating new file."
+            Write-Output "No last run date file found, therefore running the script."
             $lastRunDate = ""
-            Set-Content -Path $lastRunFile -Value $lastRunDate
         }
 
         if ($currentHour -ge 13) {
             if ($lastRunDate -ne $currentDate) {
-                if ($lastRunDate -eq $yesterdayDate) {
-                    Run-Script
-                    Update-LastRunDate -date $currentDate
-                    Write-Output "Updated last run date to: $currentDate"
-                } else {
-                    Write-Output "Last run date is not yesterday."
-                }
+                Run-Script
+                Update-LastRunDate -date $currentDate
+                Write-Output "Updated last run date to: $currentDate"
             } else {
-                Write-Output "Last run date is today or not set."
+                Write-Output "Last run date is today."
             }
         } else {
             Write-Output "It is not yet 1 PM."
