93a3b50fcd3436e5a323245bdf06ad1de86bbf30
Felix Mühlenberend <felix.muehlenberend@mailbox.org>
2024-08-28 13:24:06 +0200

    basic scraper no images, no user

--- src/scraper_facebook.py
+++ src/scraper_facebook.py
@@ -1,6 +1,10 @@
 import time
-from typing import Optional
+from typing import Optional, List, Any
 from src.util import get, overrides
+import re
+import json
+import datetime
+import pandas as pd
 
 from bs4 import BeautifulSoup
 from contextlib import contextmanager
@@ -44,13 +48,14 @@ def ignore_error(
         if DEBUG_MODE:
             raise e
 
-def get_item_urls(location, query):
-    url = URL.format(query=query)
+
+def get_browser(headless=True) -> webdriver.Chrome:
     chrome_install = ChromeDriverManager().install()
     chromedriver_path = chrome_install
     chrome_options = Options()
     # Initialize Chrome WebDriver
-    chrome_options.add_argument("--headless")
+    if headless:
+        chrome_options.add_argument("--headless")
     # required to load interactive elements in headless mode
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--disable-gpu")
@@ -60,6 +65,11 @@ def get_item_urls(location, query):
         service = Service(chromedriver_path),
         options = chrome_options
     )
+    return browser
+
+
+def get_item_urls(location: str, query: str, browser: webdriver.Chrome) -> List[str]:
+    url = URL.format(query=query)
     browser.get(url)
 
     with ignore_error("Decline button clicked!", "Could not find or click the optional cookies button!"):
@@ -142,3 +152,96 @@ def get_item_urls(location, query):
     item_urls = [FACEBOOK_BASE_URL + e for e in links if e and e.startswith('/marketplace/item/')]
     return item_urls
 
+
+def _find_key(key, obj) -> Optional[Any]:
+    if type(obj) == type({}):
+        for k in obj:
+            if k == key:
+                return obj[k]
+            res = _find_key(key, obj[k])
+            if res is not None:
+                return res
+    elif type(obj) == type([]):
+        for e in obj:
+            res = _find_key(key, e)
+            if res is not None:
+                return res
+
+
+def get_product_data(url: str, browser: webdriver.Chrome) -> Any:
+    # use selenium to load complete html (including price)
+    # browser.get(url)
+    # time.sleep(2)
+    # html = browser.page_source
+    # soup = BeautifulSoup(html, 'html.parser')
+
+    # with open('offer.html', 'w') as file:
+    #     file.write(soup.prettify())
+    #     exit()
+
+    with open('offer.html', 'r') as file:
+        soup = BeautifulSoup(file, 'html.parser')
+
+    scripts = soup.find_all('script')
+    for script in scripts:
+        json_texts = re.findall(r'{.*}', script.string)
+        for json_text in json_texts:
+            if 'price' in json_text:
+                json_data = json.loads(json_text)
+                return _find_key('target', json_data)
+    raise ValueError("Could not find product data")
+
+
+async def scrape_offer_url(url: str, browser: webdriver.Chrome) -> Offer:
+    data = get_product_data(url, browser)
+
+    offer_id = data['id']
+    offer_title = data['marketplace_listing_title']  # type: ignore
+    offer_description = data['redacted_description']
+    offer_price = data['listing_price']['amount']
+    offer_location = data['location_text']
+    timestamp = data['creation_time']
+    offer_date = datetime.datetime.utcfromtimestamp(timestamp)
+    # offer_image_urls = [img['src'] for img in soup.find_all(id='viewad-image')]
+
+    # Extract user details
+    # userprofile_vip = soup.find(class_='userprofile-vip')
+    # if userprofile_vip and userprofile_vip.a:  # type: ignore
+    #     user_link = userprofile_vip.a['href']  # type: ignore
+    #     user_id = user_link.split('=')[-1]  # type: ignore
+    #     user_name = userprofile_vip.a.text.strip()  # type: ignore
+    # else:
+    #     user_link = 'No user link'
+    #     user_id = 'No user id'
+    #     user_name = 'No user name'
+
+    # user_badge_tag = soup.find(class_='userbadge-tag')
+    # if user_badge_tag:
+    #     user_rating = user_badge_tag.text.strip()
+    # else:
+    #     user_rating = 'No rating'
+    # user_all_offers_link = BASE_URL_KLEINANZEIGEN + user_link  # type: ignore
+
+    # user = User(
+    #     id=user_id,
+    #     name=user_name,
+    #     rating=user_rating,
+    #     all_offers_link=user_all_offers_link,
+    # )
+
+    # Create data class instances
+    offer = Offer(
+        id=offer_id,
+        title=offer_title,
+        description=offer_description,
+        price=offer_price,
+        location=offer_location,
+        date=str(offer_date),
+        link=url,
+        sold=False,
+        # image_urls=[], #offer_image_urls,
+        # user=None, #user,
+        scraped_on=pd.Timestamp.now(),
+    )
+
+    return offer
