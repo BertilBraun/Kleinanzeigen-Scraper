8f076b8975c73b26db9a9ce47fd187c41718d7f0
Bertil Braun <bertil.braun.private@gmail.com>
2024-09-29 21:34:10 +0200

    Large sail filter and cleanup

--- src/config_interests.py
+++ src/config_interests.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Callable, Generic, TypeVar
+from typing import Callable, Generic, Iterable, TypeVar
 from src.types_to_search import ENTRY_TYPES, Board, Sail, Mast, Boom, FullRig, FullSet  # noqa
 
 
@@ -17,51 +17,57 @@ class InterestRequest(Generic[T]):
     filter: Callable[[T], bool] | None = None
 
 
-INTERESTED_SAIL_SIZES = ('4.9', '5.0', '5.1', '5.2', '5.8', '5.9', '6.0', '6.1', '6.2')
+INTERESTED_SAIL_SIZES = ('4.9', '5.0', '5.1', '5.2', '5.8', '5.9', '6.0', '6.1', '6.2', '8.3', '8.4', '8.5', '8.6')
+STATE_NO_GO_KEYWORDS = ('repaired', 'demaged', 'defective')
+POINT_7 = ('point-7', 'point7')
 
 
-def includes_point_7(string: str) -> bool:
-    return 'point-7' in string.lower() or 'point7' in string.lower()
+def contains(string: str, words: Iterable[str]) -> bool:
+    return any(word in string.lower() for word in words)
+
+
+def not_contains(string: str, words: Iterable[str]) -> bool:
+    return all(word not in string.lower() for word in words)
 
 
 def sail_filter(sail: Sail) -> bool:
-    if includes_point_7(sail.brand):
+    if contains(sail.brand, POINT_7):
         return True
 
-    state_ok = 'new' in sail.state.lower() or 'used' in sail.state.lower()
-    size_ok = any(size in sail.size for size in INTERESTED_SAIL_SIZES)
+    size_ok = contains(sail.size, INTERESTED_SAIL_SIZES)
+    state_ok = not_contains(sail.state, STATE_NO_GO_KEYWORDS)
 
     price = sail.metadata.price
     price_ok = True if isinstance(price, str) else (60 <= price <= 250)
+    slalom_ok = not_contains(sail.sail_type, ('slalom',))
 
-    return state_ok and size_ok and price_ok
+    return state_ok and size_ok and price_ok and slalom_ok
 
 
 def mast_filter(mast: Mast) -> bool:
     length_ok = '460' in mast.length
     sdm_ok = 'sdm' in mast.rdm_or_sdm.lower()
-    carbon_ok = any(carbon in mast.carbon for carbon in ('80', '85', '90', '95', '100'))
-
-    point_7_ok = includes_point_7(mast.brand)
+    carbon_ok = contains(mast.carbon, ('80', '85', '90', '95', '100'))
+    point_7_ok = contains(mast.brand, POINT_7)
 
     return length_ok and sdm_ok and carbon_ok and point_7_ok
 
 
 def full_set_filter(full_set: FullSet) -> bool:
-    if includes_point_7(full_set.content_description):
+    if contains(full_set.content_description, POINT_7):
         return True
 
-    sail_ok = any(size in full_set.content_description for size in INTERESTED_SAIL_SIZES)
+    sail_ok = contains(full_set.content_description, INTERESTED_SAIL_SIZES)
 
     return sail_ok
 
 
 def full_rig_filter(full_rig: FullRig) -> bool:
-    if includes_point_7(full_rig.sail.brand):
+    if contains(full_rig.sail.brand, POINT_7):
         return True
 
-    sail_ok = any(size in full_rig.sail.size for size in INTERESTED_SAIL_SIZES)
-    state_ok = 'new' in full_rig.sail.state.lower() or 'used' in full_rig.sail.state.lower()
+    sail_ok = contains(full_rig.sail.size, INTERESTED_SAIL_SIZES)
+    state_ok = not_contains(full_rig.sail.state, STATE_NO_GO_KEYWORDS)
 
     return sail_ok and state_ok
 
